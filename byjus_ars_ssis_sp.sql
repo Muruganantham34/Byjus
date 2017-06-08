7603ee943eb542b9e771f3d582a775de2a47f47d
Muruganantham <muruganantham34@gmail.com>
2017-06-08 20:14:00 +0530

    In and Out time corrected empty entry for Absent is removed
    In and Out time corrected empty entry for Absent is removed

--- byjus_ars_ssis_sp.sql
+++ byjus_ars_ssis_sp.sql
@@ -5,10 +5,6 @@ if exists ( select 'y' from sysobjects where name = 'byjus_ars_ssis_sp' and type
     drop proc byjus_ars_ssis_sp
 go
 
-
-
- 
-
 create proc byjus_ars_ssis_sp
 as
 begin
@@ -17,31 +13,35 @@ set nocount on
 declare @sysdate			date = getdate()
 		,@first_date		date
 		,@guid				udd_guid = newid()
-		,@cur_attend_date	date
+		 
 begin try
 
 --remove the below update after testing 
 		update byjus_ars_ssis_tbl
 		set    employee_code = employee_code 
 			   ,attendance_date	= cast(attendance_date as date)
-		
-		select @cur_attend_date = attendance_date
-		from   byjus_ars_ssis_tbl
 
-		delete byjus_ars_ssis_tbl where attendance_date <> @cur_attend_date 
-
-		select @first_date = DATEADD(month, DATEDIFF(month, 0, min(attendance_date)), 0) 
-		from   byjus_ars_ssis_tbl
-		
-		declare @temp table
+		create table #staging
 		(
-		master_ou				int
-		,employee_code			nvarchar(100)
-		,attendance_date		date
-		,attendance_in_time		datetime
-		,attendance_out_time	datetime
-		,guid1					nvarchar(800)
+		employee_code				nvarchar(60)
+		,attendance_date			date
+		,attendance_date_time		datetime
+		,io_flag					varchar(5)
+		)		
+
+		insert #staging
+		(
+		employee_code			
+		,attendance_date		
+		,attendance_date_time	
+		,io_flag				
 		)
+		select employee_code 
+				,attendance_date
+				,concat(a.attendance_date,space(1),a.attendance_time)
+				,io_flag
+		from  byjus_ars_ssis_tbl a
+
 
 		create table #source
 		(
@@ -63,17 +63,19 @@ begin try
 		,shift_tolerance_limit  int
 		)
 		
+		insert #source
+		(
+		master_ou		
+		,employee_code	
+		,attendance_date
+		,guid
+		)
 		
-		insert @temp (master_ou
-					,employee_code
-					,attendance_date
-					,guid1
-					)
-		select	asgn.master_ou_code
-				,a.employee_code
-				,a.attendance_date
-				,newid()
-		from    byjus_ars_ssis_tbl a 
+		select asgn.master_ou_code
+			   ,a.employee_code			
+			   ,a.attendance_date
+			   ,newid()
+		from   #staging a
 		join	hrei_asgn_eff_auth_dtl (nolock) asgn
 				on     a.employee_code = asgn.employee_code
 				and    asgn.assignment_auth_status = 'A'
@@ -81,111 +83,97 @@ begin try
 		group by asgn.master_ou_code
 				,a.employee_code
 				,a.attendance_date
-		
+
+
+		 
 		;with attendance_in_cte as
 		(
-		select  master_ou_code
-				,employee_code
+		select   employee_code
 				,attendance_date
 				,min(attendance_in_time) 'attendance_in_time'
 		from 
 		(
-		select	asgn.master_ou_code
-				,a.employee_code
-				,a.attendance_date
-				,cast(concat(a.attendance_date,space(1),a.attendance_time) as datetime)   attendance_in_time
-		from    byjus_ars_ssis_tbl a 
-		join	hrei_asgn_eff_auth_dtl (nolock) asgn
-				on     a.employee_code = asgn.employee_code
-				and    asgn.assignment_auth_status = 'A'
-				and    a.attendance_date between assignment_effective_from_date	and isnull(asgn.assignment_effective_to_date,a.attendance_date)
-		where   a.io_flag = 'i'
+
+		select  employee_code			
+				,attendance_date		
+				,attendance_date_time	'attendance_in_time'
+		from    #staging
+		where	io_flag = 'i'
+		
 		union all
-		select t.master_ou,t.employee_code,t.tmsht_date,tmsht_from_date
-						
+		
+		select t.employee_code,t.tmsht_date,tmsht_from_date
 		from   tmsht_hourly_based_dtl t (nolock)
-		join   byjus_ars_ssis_tbl s 
+		join   #staging s 
 				on t.employee_code = s.employee_code
 				and t.tmsht_date = s.attendance_date
 
 		
 		) a
-		group by a.master_ou_code
-				,a.employee_code
+		group by a.employee_code
 				,a.attendance_date
 		)
 
 		update a
 		set    attendance_in_time = b.attendance_in_time
-		from   @temp a 
+		from   #source a 
 		join   attendance_in_cte b
-				on  a.master_ou		  = b.master_ou_code
-				and a.employee_code	  = b.employee_code
+				on  a.employee_code	  = b.employee_code
 				and a.attendance_date = b.attendance_date
 		
 		;with attendance_out_cte as
 		(
-		select  master_ou_code
-				,employee_code
+		select   employee_code
 				,attendance_date
 				,max(attendance_out_time) 'attendance_out_time'
 		from 
 		(
-		select	asgn.master_ou_code
-				,a.employee_code
-				,a.attendance_date
-				,cast(concat(a.attendance_date,space(1),a.attendance_time) as datetime)   attendance_out_time
-		from    byjus_ars_ssis_tbl a 
-		join	hrei_asgn_eff_auth_dtl (nolock) asgn
-				on     a.employee_code = asgn.employee_code
-				and    asgn.assignment_auth_status = 'A'
-				and    a.attendance_date between assignment_effective_from_date	and isnull(asgn.assignment_effective_to_date,a.attendance_date)
-		where   a.io_flag = 'o'
+
+		select  employee_code			
+				,attendance_date		
+				,attendance_date_time	'attendance_out_time'
+		from    #staging
+		where	io_flag = 'o'
+		
 		union all
-		select t.master_ou,t.employee_code,t.tmsht_date,tmsht_from_date
-						
+		
+		select t.employee_code,t.tmsht_date,tmsht_to_date
 		from   tmsht_hourly_based_dtl t (nolock)
-		join   byjus_ars_ssis_tbl s 
+		join   #staging s 
 				on t.employee_code = s.employee_code
 				and t.tmsht_date = s.attendance_date
 
 		
 		) a
-		group by a.master_ou_code
-				,a.employee_code
+		group by a.employee_code
 				,a.attendance_date
+		
 		)
 		
 		update a
 		set    attendance_out_time = b.attendance_out_time
-		from   @temp a 
+		from   #source a 
 		join   attendance_out_cte b
-				on  a.master_ou		  = b.master_ou_code
-				and a.employee_code	  = b.employee_code
+				on  a.employee_code	  = b.employee_code
 				and a.attendance_date = b.attendance_date
 
-
+ 
 		;with attendance_max_in_cte as
 		(
-		select  master_ou_code
-				,employee_code
+		select   employee_code
 				,attendance_date
 				,max(attendance_in_time) 'attendance_in_time'
 		from 
 		(
-		select	asgn.master_ou_code
-				,a.employee_code
-				,a.attendance_date
-				,cast(concat(a.attendance_date,space(1),a.attendance_time) as datetime)   attendance_in_time
-		from    byjus_ars_ssis_tbl a 
-		join	hrei_asgn_eff_auth_dtl (nolock) asgn
-				on     a.employee_code = asgn.employee_code
-				and    asgn.assignment_auth_status = 'A'
-				and    a.attendance_date between assignment_effective_from_date	and isnull(asgn.assignment_effective_to_date,a.attendance_date)
-		where   a.io_flag = 'i'
+		select  employee_code			
+				,attendance_date		
+				,attendance_date_time	'attendance_in_time'
+		from    #staging
+		where	io_flag = 'i'
+		
 		union all
-		select t.master_ou,t.employee_code,t.tmsht_date,tmsht_from_date
-						
+		
+		select t.employee_code,t.tmsht_date,tmsht_to_date
 		from   tmsht_hourly_based_dtl t (nolock)
 		join   byjus_ars_ssis_tbl s 
 				on t.employee_code = s.employee_code
@@ -193,70 +181,23 @@ begin try
 
 		
 		) a
-		group by a.master_ou_code
-				,a.employee_code
+		group by a.employee_code
 				,a.attendance_date
 		)
 
+
 		update a
 		set    attendance_out_time = b.attendance_in_time
-		from   @temp a 
+		from   #source a 
 		join   attendance_max_in_cte b
-				on  a.master_ou		  = b.master_ou_code
-				and a.employee_code	  = b.employee_code
+				on  a.employee_code	  = b.employee_code
 				and a.attendance_date = b.attendance_date
 				and b.attendance_in_time > a.attendance_out_time 
-
-		if exists ( select '%' 
-					from   tmsht_hourly_based_dtl (nolock) 
-					where  tmsht_date = @first_date 
-					)
-		begin
-				select ''
-		end
-		else
-		begin	
-
-				with daysinmonth as 
-				(
-					select @first_date as date
-					union all
-					select dateadd(dd,1,date)
-					from daysinmonth
-					where month(date) = month(@first_date)
-				)
-
-				insert #source (guid,master_ou,employee_code,attendance_date)
-				select  
-				newid()
-				,asgn.master_ou_code
-			   ,asgn.employee_code
-			   ,date
-				from   hrei_asgn_eff_auth_dtl (nolock) asgn
-				       ,daysinmonth 
-				where  month(date) = month(@first_date)
-				and    asgn.assignment_auth_status = 'A'
-				and    asgn.assignment_effective_to_date is null
-
 		
-		end 
+ 
+		update #source
+		set    regular_hours = (datediff(ss,attendance_in_time,isnull(attendance_out_time,attendance_in_time)) / 3600.00) * 60
 
-		merge into #source as t
-			using	@temp	as	s
-					on		t.employee_code = s.employee_code
-					and		t.attendance_date	  = s.attendance_date
-			when	matched	then
-					update	set	 t.attendance_in_time		=	s.attendance_in_time
-								,t.attendance_out_time		=	s.attendance_out_time
-								,t.regular_hours			=	(datediff(ss,s.attendance_in_time,isnull(s.attendance_out_time,s.attendance_in_time)) / 3600.00) * 60
-			when	not	matched	then
-					insert
-					(guid ,master_ou,employee_code        , attendance_date          , attendance_in_time			    , attendance_out_time    , regular_hours)
-					values
-					(
-					s.guid1,s.master_ou,s.employee_code			   ,s.attendance_date					,s.attendance_in_time				,s.attendance_out_time
-					,(datediff(ss,s.attendance_in_time,isnull(s.attendance_out_time,s.attendance_in_time)) / 3600.00) * 60)
-					;
 		---Update the shift code for each employees
 
 		update	tmp
@@ -381,7 +322,7 @@ begin try
 				,s.attendance_in_time ,s.attendance_out_time ,s.regular_hours ,s.ot_hours			,s.late_coming_hours ,s.early_going_hours , s.shift_code
 				);
  
--- 		truncate table byjus_ars_ssis_tbl
+ 		truncate table byjus_ars_ssis_tbl
 
 end try
 begin catch
